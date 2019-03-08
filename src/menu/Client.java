a281720bf93dce720e49f2b0d51065d5c7ec37c1
Aviel Cohen <acohen41@students.towson.edu>
2019-03-08 14:40:26 -0500

    made display better

--- src/menu/Client.java
+++ src/menu/Client.java
@@ -26,9 +26,9 @@ public class Client {
 		MenuItem item;
 		
 		System.out.println("#####################################");
-		System.out.println("Option 1: Please select a price and the itmes at or below that price will be displayed.");
+		System.out.println("Option 1: Please select a price and the items at or below that price will be displayed.");
 		System.out.println("Option 2: Display heart healthy options.");
-		System.out.println("Option 3: Please select a category of a dish and the itmes in that category will be displayed.");
+		System.out.println("Option 3: Please select a category of a dish and the items in that category will be displayed.");
 		System.out.println("Option 4: Display all items in the list.");
 		System.out.println("#####################################");
 		
@@ -70,6 +70,11 @@ public class Client {
 		
 		case 3:
 			System.out.println("Select a category:");
+			System.out.println("#####################################");
+			System.out.println("Option 1: Appetizers");
+			System.out.println("Option 2: Main Dishes");
+			System.out.println("Option 3: Desserts");
+			System.out.println("#####################################");
 			int category = input.nextInt();
 			itr = eatAtJoesMenu.getItemIterator(category);
 				while(itr.hasNext()) {
@@ -90,7 +95,7 @@ public class Client {
 				
 				item = itr.next();
 				System.out.println(item.getName());
-				System.out.println("Do you wish to delete this item");
+				System.out.println("Do you wish to delete this item, if yes hit d.");
 				delete = input.next();
 				if(delete.contentEquals("d"))
 					eatAtJoesMenu.delete(item);
